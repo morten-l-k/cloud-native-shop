d6946978c40bb4cd75b26930a9524f94c5087928
Christian Riisberg <riisberg@Christians-MacBook-Pro.local>
2026-02-27 20:02:59 +0100

    added angular to project and removed old frontend

--- frontend/src/pages/ProfilePage.tsx
+++
@@ -1,8 +0,0 @@
-export function ProfilePage() {
-    return (
-        <div>
-            <h1>Your Profile:</h1>
-            <p> Hello Vinz!</p>
-        </div>
-    );
-}
--- frontend/src/shared/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer style={{ borderTop: "1px solid #ddd", padding: "16px 20px" }}>
-      <div style={{ maxWidth: 1200, margin: "0 auto", fontSize: 14 }}>
-        © {new Date().getFullYear()} MyShop
-      </div>
-    </footer>
-  );
-}
