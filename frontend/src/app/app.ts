c2b31a1e6c42bb46c2351a66992ea9dd1b048fef
Christian Riisberg <riisberg@Christians-MacBook-Pro.local>
2026-03-01 17:56:38 +0100

    test

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true
 })
 export class App {
-  protected readonly title = signal('frontend');
+  // 1. A variable we can show in the HTML
+  shopName = 'Cloud Native Shop';
+
+  // 2. A function we can trigger from the HTML
+  testButton() {
+    alert('Clicked a button');
+  }
 }
