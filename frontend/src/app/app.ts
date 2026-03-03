3b692df398efecea7108886aeebb2481a51b8ca4
Christian Riisberg <riisberg@Christians-MacBook-Pro.local>
2026-03-03 02:00:10 +0100

    a few updates

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { CloudIcon } from './components/cloud-icon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  standalone: true
+  standalone: true,
+  imports: [CloudIcon]
 })
 export class App {
-  // 1. A variable we can show in the HTML
-  shopName = 'Cloud Native Shop';
+  // 1. We split the name to insert the icon in the HTML
+  shopNamePrefix = 'Cloud Nat';
+  shopNameSuffix = 've Shop';
 
   // 2. A function we can trigger from the HTML
   testButton() {
