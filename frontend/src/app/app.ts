4749a5e9c1ccf622d3113b4d4a1e29f578659a25
Oskar <xqd627@alumni.ku.dk>
2026-03-09 15:48:13 +0100

    init

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './global/navbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MatIconModule]
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    NavbarComponent,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule
+  ]
 })
 export class App {
   shopNamePrefix = 'Cloud Nat';
   shopNameSuffix = 've Shop';
+
+  categories = ['Category1', 'Category2', 'Category3', 'Category4'];
 }
